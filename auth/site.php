92eb1601f290eb27d39ac4ffb74ade1b93c189be
khaironbiz <khaironbiz@gmail.com>
2021-11-02 05:58:41 +0700

    ovon

---
+++ auth/site.php
@@ -0,0 +1,14 @@
+<?php
+//site local
+$site_url           = "http://localhost/ovon";
+//$site_url           = "https://ppni.rspon.net/simk";
+$nama_web           = "DPK PPNI RSPON";
+$nama_perusahaan    = "DPK PPNI RSPON";
+$alamat_perusahaan  = "Jl. MT Haryono Kavling 11 Cawang, Jakarta Timur, DKI Jakarta 13630";
+$deskripsi_web      = "Sistem Informasi Manajemen Anggota";
+$version_web        = "2.0.0";
+?>
+<script>
+    var base_url = "http://localhost/ovon/";
+    //var base_url = "https://ppni.rspon.net/simk";
+</script>
---
+++ login.php
@@ -0,0 +1,83 @@
+<?php
+include("auth/site.php");
+include("auth/login.php");
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Login Area</title>
+  <link rel="icon" href="https://ppni.or.id/simk/image/LOGO-PPNI.png">
+
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="<?= $site_url;?>/assets/AdminLTE/plugins/fontawesome-free/css/all.min.css">
+  <!-- icheck bootstrap -->
+  <link rel="stylesheet" href="<?= $site_url;?>/assets/AdminLTE/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="<?= $site_url;?>/assets/AdminLTE/dist/css/adminlte.min.css">
+</head>
+<body class="hold-transition login-page">
+<div class="login-box">
+  <div class="login-logo">
+    <a href="<?= $site_url ?>"><b>PPNI</b>RSPON</a>
+  </div>
+  <!-- /.login-logo -->
+  <div class="card">
+    <div class="card-body login-card-body">
+      <p class="login-box-msg">Sign in to start your session</p>
+
+      <form action="" method="post">
+        <div class="input-group mb-3">
+          <input type="email" class="form-control" placeholder="Email" name="username">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-envelope"></span>
+            </div>
+          </div>
+        </div>
+        <div class="input-group mb-3">
+          <input type="password" class="form-control" placeholder="Password" name="password">
+          <div class="input-group-append">
+            <div class="input-group-text">
+              <span class="fas fa-lock"></span>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-8">
+            <div class="icheck-primary">
+              <input type="checkbox" id="remember">
+              <label for="remember">
+                Remember Me
+              </label>
+            </div>
+          </div>
+          <!-- /.col -->
+          <div class="col-4">
+            <button type="submit" class="btn btn-primary btn-block">Sign In</button>
+          </div>
+          <!-- /.col -->
+        </div>
+      </form>
+
+      <p class="mb-1">
+        <a href="<?= $site_url;?>/auth/forgot.php">I forgot my password</a>
+      </p>
+    </div>
+    <!-- /.login-card-body -->
+  </div>
+</div>
+<!-- /.login-box -->
+
+<!-- jQuery -->
+<script src="<?= $site_url;?>/assets/AdminLTE/plugins/jquery/jquery.min.js"></script>
+<!-- Bootstrap 4 -->
+<script src="<?= $site_url;?>/assets/AdminLTE/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<!-- AdminLTE App -->
+<script src="<?= $site_url;?>/assets/AdminLTE/dist/js/adminlte.min.js"></script>
+</body>
+</html>
